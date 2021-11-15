b90a3508bcb7b1804221ed8a8822e5a94ad7038b
lisamurphy788 <lisamurphy788@gmail.com>
2021-11-15 15:32:54 +0000

    Add mobal to html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,56 @@
 
 const cards = document.querySelectorAll('.card');
-const timeContainer = document.querySelector('.timer')
-const rules = document.getElementById("modal"); // Get the button that opens the modal
-const btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];// Get the <span> element that closes the modal
+//const rules = document.getElementById('rules')
+//const modal = document.getElementById ('modal')
+//const rulesBtn = document.getElementById('rulesBtn')
+//const closeBtn = document.getElementById ('closeBtn')
+//const timeContainer = document.querySelector('.timer')
+//const rules = document.getElementById("modal"); // Get the button that opens the modal
+//const btn = document.getElementById("myBtn");
+//var span = document.getElementsByClassName("close")[0];// Get the <span> element that closes the modal
 
 
 let hasFlippedCard = false;//if card was clicked already
 let lockBoard = false;//lock board
 let firstCard, secondCard; //cards match
 
+// Get the modal
+var modal = document.getElementById("rulesModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+//rulesBtn.addEventListener('click', showRules);
+//closeBtn.addEventListener('click', closeRules);
+
+////function showRules() {
+    //modal.style.display = "block";
+
+//}
+
+//function closeRules() {
+    //modal.style.display = "none";
+//}
+/*
  //timer
  let time;
  let minutes = 0;
@@ -33,10 +73,10 @@ let firstCard, secondCard; //cards match
  function stopTime() {
      clearInterval(time);
  }
-
+*/
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+/*btn.onclick = function() {
     modal.style.display = "block";
   }
   
@@ -51,12 +91,12 @@ btn.onclick = function() {
       modal.style.display = "none";
     }
   }
-
+*/
 
 function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
-    timer();
+   // timer();
 
 
     this.classList.add('flip');
