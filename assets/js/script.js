6e5dfc6bcfc44b98fb091dafdf4405e229909861
lisamurphy788 <lisamurphy788@gmail.com>
2021-11-28 23:10:09 +0000

    Put through CSS and jshint validator for errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,11 @@ var modal = document.getElementById("rulesModal"); // Get the modal
 var btn = document.getElementById("myBtn"); // Get the button that opens the modal
 var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
 var flipContainer = document.querySelector(".flips");
-const gameOver = document.getElementById("gameOver")
+const gameOver = document.getElementById("gameOver");
 
 
-const allMatch = 6;
-let aMatch = 0
-//let ismatch = 0;
-//const timer = document.getElementsByClassName("timer")
-//const gameOver = document.getElementById("game-over-modal")
-
+const allMatch = 6; // Shows the total number of pairs 
+let aMatch = 0; // cards match 
 
 
 let hasFlippedCard = false; //if card was clicked already
@@ -25,20 +21,20 @@ let flips = 0;
 // When the user clicks the button, open the modal 
 btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
 
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 //Flips 
 
 flips = 0;
@@ -66,7 +62,7 @@ function flipCard() {
     return;
   }
   //second click
-  secondCard = this
+  secondCard = this;
   checkForMatch();
 }
 
@@ -75,13 +71,10 @@ function checkForMatch() {
   let ismatch = firstCard.dataset.image === secondCard.dataset.image;
   if (ismatch) aMatch += 1; // every matched card
 
-  if (ismatch) disableCards()
-  else unflipCards()
+  if (ismatch) disableCards();
+  else unflipCards();
 
-  if (aMatch === allMatch) win() //{ // all cards are matched
-  //alert('You win');
-
-  // }
+  if (aMatch === allMatch) win(); 
 }
 // Show Win message 
 function win() {
@@ -95,8 +88,8 @@ const close = document.querySelector(".modal-win span");
 close.onclick = () => document.getElementById("gameOver").style.display = "none";
 
 function disableCards() {
-  firstCard.removeEventListener('click', flipCard)
-  secondCard.removeEventListener('click', flipCard)
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
   resetBoard();
 }
 
@@ -119,10 +112,9 @@ function resetBoard() {
 
 // Play Again Reset 
 
-function reset ()
-{
+function reset () {
   setTimeout(() => {
-  flippedCard = false;
+  hasFlippedCard = false;
   flips = 0;
   flipContainer.innerHTML = 0;
   [firstCard, secondCard] = [null, null];
@@ -138,7 +130,7 @@ function reset ()
 
   function shuffle() {
     cards.forEach(cards => {
-        let randomPosition = Math.floor(Math.random() * 16);
+        let randomPosition = Math.floor(Math.random() * 12);
         cards.style.order = randomPosition;
     });
 
