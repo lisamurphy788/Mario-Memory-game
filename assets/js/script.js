0a456bf50ac0c710f5f9b0791bcd9a274df99d34
lisamurphy788 <lisamurphy788@gmail.com>
2021-11-28 09:47:15 +0000

    Add close button to modal and working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 const cards = document.querySelectorAll('.card');
-var modal = document.getElementById("rulesModal");// Get the modal
-var btn = document.getElementById("myBtn");// Get the button that opens the modal
+var modal = document.getElementById("rulesModal"); // Get the modal
+var btn = document.getElementById("myBtn"); // Get the button that opens the modal
 var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
 var flipContainer = document.querySelector(".flips");
 const gameOver = document.getElementById("gameOver")
+
 
 const allMatch = 6;
 let aMatch = 0
@@ -13,8 +14,8 @@ let aMatch = 0
 
 
 
-let hasFlippedCard = false;//if card was clicked already
-let lockBoard = false;//lock board
+let hasFlippedCard = false; //if card was clicked already
+let lockBoard = false; //lock board
 let firstCard, secondCard; //cards match
 let flips = 0;
 
@@ -22,18 +23,18 @@ let flips = 0;
 //modal adapted from https://www.w3schools.com/howto/howto_css_modals.asp
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-  
+
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
@@ -45,74 +46,71 @@ flipContainer.innerHtml = 0;
 
 
 function addFlip() {
-    flips++;
-    flipContainer.innerHTML = flips;
+  flips++;
+  flipContainer.innerHTML = flips;
 }
 
 function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
-    addFlip();
-    
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  addFlip();
 
 
-    this.classList.add('flip');
 
-    if (!hasFlippedCard) {
-        //first Click
-        hasFlippedCard = true;
-        firstCard = this;   
-        return;
-    }
-        //second click
-    secondCard = this
-    checkForMatch();
-}   
+  this.classList.add('flip');
 
-function checkForMatch () {
-    //stay facing upwards 
-    let ismatch = firstCard.dataset.image === secondCard.dataset.image;
-    if (ismatch) aMatch += 1; // every matched card
+  if (!hasFlippedCard) {
+    //first Click
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  //second click
+  secondCard = this
+  checkForMatch();
+}
 
-    if (ismatch) disableCards ()
-    else unflipCards ()
-   
-    if (aMatch === allMatch) win () //{ // all cards are matched
-      //alert('You win');
-     
-    // }
+function checkForMatch() {
+  //stay facing upwards 
+  let ismatch = firstCard.dataset.image === secondCard.dataset.image;
+  if (ismatch) aMatch += 1; // every matched card
+
+  if (ismatch) disableCards()
+  else unflipCards()
+
+  if (aMatch === allMatch) win() //{ // all cards are matched
+  //alert('You win');
+
+  // }
 }
 // Show Win message 
-function win () {
-    gameOver.style.display = "block";
-}
-span.onclick = function(event) {
-
-  if(event.target.class == "close"){
-  document.getElementsById("gameOver").style.display = "none"
-  }
-  
+function win() {
+  gameOver.style.display = "block";
 }
 
-function disableCards () {
-    firstCard.removeEventListener('click', flipCard)
-    secondCard.removeEventListener('click', flipCard)
-    resetBoard();
+// Close out of win Modal 
+const close = document.querySelector(".modal-win span");
+close.onclick = () => document.getElementById("gameOver").style.display = "none";
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard)
+  secondCard.removeEventListener('click', flipCard)
+  resetBoard();
 }
 
 function unflipCards() {
-    lockBoard = true;
+  lockBoard = true;
 
-    setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
     resetBoard();
-    }, 1000);
+  }, 1000);
 }
 
-function resetBoard () {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 //Timer
@@ -128,10 +126,10 @@ function resetBoard () {
   }*/
 
 (function shuffle() {
-    cards.forEach(card => {
-        let randomPosition = Math.floor(Math.random() * 12);
-        card.style.order = randomPosition;
-    });
+  cards.forEach(card => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
 
 })();
 
